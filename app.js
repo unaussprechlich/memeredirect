baa6d014d6d442c725f236e8716b355114ff429d
unaussprechlich@pc <keck.alexander@web.de>
2018-02-06 20:45:49 +0100

    fix

--- app.js
+++ app.js
@@ -82,6 +82,8 @@ app.use(function (req, res, next) {
     next(404);
 });
 app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
     res.render("error.pug");
 });
--- app.ts
+++ app.ts
@@ -91,11 +91,18 @@ app.get("/", async function (req, res, next) {
     }
 });
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(404);
 });
 
+// error handler
 app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
     res.status(err.status|| 500);
     res.render("error.pug");
 });
